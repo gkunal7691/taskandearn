105e28d66f886d862f625ec71d773127140b054f
Gaurav Kunal <gaurav@capbpm.com>
2020-07-04 14:40:20 +0530

    no chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
