7bf77a04eb2935035789788ea71e44e5753c4340
akshay-4362 <akshay.t@softobotics.com>
2020-07-09 16:27:55 +0530

    services slider

--- routes/core/auth.js
+++ routes/core/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 var passport = require('passport');
 const User = require('../../models').User
 const config = require('../../config/config');
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
 const request = require('request');
 const uuidv1 = require('uuid/v1');
@@ -68,47 +68,47 @@ router.post('/login', function (req, res, next) {
 });
 
 // Password Reset mail
-router.delete('/:email/:orgId', async function (req, res, next) {
-    const query = {};
-    let url = 'href="https://' + req.headers.host + '/login/resetpassword/';
+// router.delete('/:email/:orgId', async function (req, res, next) {
+//     const query = {};
+//     let url = 'href="https://' + req.headers.host + '/login/resetpassword/';
 
-    query.where = { email: req.params.email, organizationId: req.params.orgId };
+//     query.where = { email: req.params.email, organizationId: req.params.orgId };
 
-    User.findOne(query).then((users) => {
-        let token = jwt.sign({
-            data: users
-        }, config.jwt.secret, { expiresIn: 60 * 60 });
-        let link = url + token;
-        let transporter = nodemailer.createTransport({
-            host: "mail.softobotics.com",
-            port: 465,
-            secure: true,
-            auth: {
-                user: 'notification@softobotics.com',
-                pass: 'notification@123'
-            }
-        });
-        var mailOptions = {
-            from: 'notification@softobotics.com',
-            to: users.email,
-            subject: 'TMS- Reset You Password',
-            html: '<p><b>Dear ' + users.firstName + ' ' + users.lastName + '</b></p><br><p>You recently requested to reset your password from the TMS App. Click the link below to complete the process.<p><br/><button style="background-color: #EF7745;border-radius: 6px;color: white;text-decoration: none;display: inline-block;font-size: 14px;padding: 15px 32px;border: none;"><a style="text-decoration: none;color:#fff" '
-                + link + '">Reset Your Password</a></button><p style="font-family: Arial, sans-serif; font-size: 14px; color: #232740">Sincerely, <br>Team TMS</p>'
-        };
-        transporter.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
-                res.json({ success: false, data: error });
+//     User.findOne(query).then((users) => {
+//         let token = jwt.sign({
+//             data: users
+//         }, config.jwt.secret, { expiresIn: 60 * 60 });
+//         let link = url + token;
+//         let transporter = nodemailer.createTransport({
+//             host: "mail.softobotics.com",
+//             port: 465,
+//             secure: true,
+//             auth: {
+//                 user: 'notification@softobotics.com',
+//                 pass: 'notification@123'
+//             }
+//         });
+//         var mailOptions = {
+//             from: 'notification@softobotics.com',
+//             to: users.email,
+//             subject: 'TMS- Reset You Password',
+//             html: '<p><b>Dear ' + users.firstName + ' ' + users.lastName + '</b></p><br><p>You recently requested to reset your password from the TMS App. Click the link below to complete the process.<p><br/><button style="background-color: #EF7745;border-radius: 6px;color: white;text-decoration: none;display: inline-block;font-size: 14px;padding: 15px 32px;border: none;"><a style="text-decoration: none;color:#fff" '
+//                 + link + '">Reset Your Password</a></button><p style="font-family: Arial, sans-serif; font-size: 14px; color: #232740">Sincerely, <br>Team TMS</p>'
+//         };
+//         transporter.sendMail(mailOptions, function (error, info) {
+//             if (error) {
+//                 console.log(error);
+//                 res.json({ success: false, data: error });
 
-            } else {
-                console.log('Email sent: ' + info);
-                res.json({ success: true, data: info });
+//             } else {
+//                 console.log('Email sent: ' + info);
+//                 res.json({ success: true, data: info });
 
-            }
-        });
-    }).catch(next)
+//             }
+//         });
+//     }).catch(next)
 
-});
+// });
 
 //reset password
 router.patch('/', function (req, res, next) {
