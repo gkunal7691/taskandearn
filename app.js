74515f280b57203b6988e71d3b132fb0816d0673
Gaurav <gaurav@capbpm.com>
2020-07-09 18:37:28 +0530

    app.js

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ const originsWhitelist = [
 
 originsWhitelist.push('http://localhost:4200');
 originsWhitelist.push('http://localhost:8000');
-originsWhitelist.push('https://taskandearn.softobotics.com');
 originsWhitelist.push('https://taskandearn.herokuapp.com');
 
 app.use(logger('dev'));
