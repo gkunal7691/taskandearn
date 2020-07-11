e0c9b8668f1cc8e2b9258d0977a9463a463ca34e
akshay-4362 <akshay.t@softobotics.com>
2020-07-11 20:04:17 +0530

    spelling mistak

--- models/core/user.js
+++ models/core/user.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        iaProfessional: {
+        isProfessional: {
             type: DataTypes.BOOLEAN,
             allowNull: false
         },
