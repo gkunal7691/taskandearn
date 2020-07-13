6cdd4bb0e2e123dc8b79c50eee96bce2351474e2
akshay-4362 <akshay.t@softobotics.com>
2020-07-13 14:40:52 +0530

    working on subcat page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { from } from 'rxjs';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { JoinAsProComponent } from './join-as-pro/join-as-pro.component';
+import { SubCategoryComponent } from './sub-category/sub-category.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
@@ -12,6 +13,9 @@ const routes: Routes = [
   { path: 'joinaspro', component: JoinAsProComponent },
 
   { path: 'registration', component: RegistrationPageComponent },
+  // { path: 'registration', component: RegistrationPageComponent },
+  { path: 'subcategory', component: SubCategoryComponent },
+
 
 
 ];
