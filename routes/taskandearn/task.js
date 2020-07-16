e73c6dc1b8e399e3cefa14d7dd906bb5cb65203a
Gaurav <gaurav@capbpm.com>
2020-07-16 23:38:36 +0530

    pro & task get api

--- routes/taskandearn/task.js
+++ routes/taskandearn/task.js
@@ -7,6 +7,29 @@ const Address = require('../../models/').Address
 const SubCategory = require('../../models/').SubCategory
 const Category = require('../../models/').Category
 
+router.get('/task/:categoryId/:text', async function (req, res, next) {
+    console.log(req.params)
+    Task.findAll({
+        where: {
+            categoryId: req.params.categoryId,
+            $or: [
+                {
+                    title: {
+                        $like: '%' + req.params.text + '%'
+                    },
+                },
+                {
+                    description: {
+                        $like: '%' + req.params.text + '%'
+                    },
+                },
+            ]
+        }
+    }).then((data) => {
+        res.json({ success: true, data: data });
+    }).catch(next)
+});
+
 router.post('/', async function (req, res, next) {
     let x = req.body;
     Address.create({
--- src/app/hire-pro/hire-pro.component.ts
+++ src/app/hire-pro/hire-pro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProfessionalsService } from '../services/professionals.service';
 
 @Component({
   selector: 'app-hire-pro',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hire-pro.component.css']
 })
 export class HireProComponent implements OnInit {
-
-  constructor() { }
+  categoryId;
+  text;
+  constructor(private route: ActivatedRoute, private professionService: ProfessionalsService) {
+    this.categoryId = this.route.snapshot.queryParams["categoryId"];
+    this.text = this.route.snapshot.queryParams["text"];
+  }
 
   ngOnInit(): void {
+    this.categoryId = parseInt(this.route.snapshot.queryParams["categoryId"]);
+    this.text = this.route.snapshot.queryParams["text"];
+    this.getSearchedProfessional();
+  }
+
+  getSearchedProfessional() {
+    this.professionService.getSearchedProfessionals(this.categoryId, this.text).subscribe(res => {
+      console.log(res)
+    })
   }
 
 }
