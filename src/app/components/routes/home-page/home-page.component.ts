e4690107f7c5d70411418a1deb2a3dd62789e55f
AMIT VERMA <amit.verma@softobotics.com>
2020-12-12 20:02:53 +0530

    owl-carousel added

--- src/app/components/routes/home-page/home-page.component.ts
+++ src/app/components/routes/home-page/home-page.component.ts
@@ -5,6 +5,7 @@ import { ProfessionalsService } from '../../../services/professionals.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CacheService } from '../../../services/cache.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 
 @Component({
@@ -108,5 +109,28 @@ export class HomePageComponent implements OnInit {
       this.professional = true
     }
   }
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 600,
+    navText: ['&#8249', '&#8250;'],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 3
+      }
+    },
+    nav: true
+  }
 }
