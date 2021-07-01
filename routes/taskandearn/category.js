352f18f6f2c9af60e7076667d0a163acd84b4164
Gaurav Kunal <gaurav@getproven.com>
2021-07-01 23:37:04 +0530

    icon col added in category

--- routes/taskandearn/category.js
+++ routes/taskandearn/category.js
@@ -1,20 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const utils = require('../../config/utils');
 const Category = require('../../models').Category;
 const PopService = require('../../models').PopularService
 
 
-
 router.post('/', async (req, res, next) => {
-    // console.log('working', req.body)
-
     Category.create({
         imagePath: req.body.imagePath,
         categoryName: req.body.categoryName,
         description: req.body.description
     }).then(data => {
-        res.json({ success: true, data: data })
+        res.json({ success: true, data: data });
     }).catch(next)
 })
 
